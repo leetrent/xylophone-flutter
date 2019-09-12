708819e3288bb171ed02f91c5ddadff97884d114
LeeTrent <lee.trent.1@gmail.com>
2019-09-11 22:31:26 -0500

    Removes commented-out code

--- lib/main.dart
+++ lib/main.dart
@@ -3,26 +3,6 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(XylophoneApp());
 
-//class XylophoneApp extends StatelessWidget {
-//  @override
-//  Widget build(BuildContext context) {
-//    return MaterialApp(
-//      home: Scaffold(
-//        body: SafeArea(
-//          child: Center(
-//            child: FlatButton(
-//              onPressed: () {
-//                final player = AudioCache();
-//                player.play('note1.wav');
-//              },
-//              child: Text('Click me!'),
-//            ),
-//          ),
-//        ),
-//      ),
-//    );
-//  }
-//}
 class XylophoneApp extends StatelessWidget {
   void playSound(int nbr) {
     final player = AudioCache();
